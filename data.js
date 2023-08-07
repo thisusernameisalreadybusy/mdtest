b771ed8af19d97c1957c8593f7c3a77135701c1a
Nurik Kosimov <85331335+NursultanKosimov@users.noreply.github.com>
2023-08-08 01:42:58 +0600

    initial commitr

---
+++ data.js
@@ -0,0 +1,8 @@
+const reportData = [
+	["Project A", 1000.0, "internal", 200.0, 300.0],
+	["Project B", 1500.0, "commercial", 250.0, 400.0],
+	["Project C", 800.0, "internal", 180.0, 350.0],
+	["Project D", 2000.0, "commercial", 300.0, 500.0],
+	["Project E", 1200.0, "internal", 220.0, 380.0],
+	// Добавьте еще строки данных по аналогии
+ ];
