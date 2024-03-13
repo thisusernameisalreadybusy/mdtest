abc29626ed3b6b38274e26eca1a6deb44bb1354b
Vladimir <bored@icloud.com>
2024-03-14 00:15:17 +0500

    fdsf

---
+++ Sources/ContentView/ContentView.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+@available(iOS 15.0.0, *)
+public struct ContentView: View {
+    @available(iOS 15.0.0, *)
+    public var body: some View {
+        Text("I am working!")
+    }
+}
