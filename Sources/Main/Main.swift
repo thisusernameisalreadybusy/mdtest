cfeb9987cec3fd343b319280f14d29300357b92a
Vladimir <thisusernameisalreadybusy@gmail.com>
2024-03-13 23:27:01 +0500

    chore: scaffold basic iOS application

---
+++ Sources/Main/Main.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+import ContentView
+
+@main
+fileprivate struct Main: App {
+    fileprivate var body: some Scene {
+        WindowGroup() {
+            ContentView()
+        }
+    }
+}
