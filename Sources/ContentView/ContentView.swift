5dbda9c455fa537c48e07f27f9b92a9838727c8c
Vladimir <bored@icloud.com>
2024-03-14 01:05:54 +0500

    fix

--- Sources/ContentView/ContentView.swift
+++ Sources/ContentView/ContentView.swift
@@ -2,6 +2,10 @@ import SwiftUI
 
 @available(iOS 15.0.0, macOS 10.15.0, *)
 public struct ContentView: View {
+    public init() {
+
+    }
+
     public var body: some View {
         Text("I am working!")
     }
